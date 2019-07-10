9afd390f2d3719811dd54da589059ced23048ea2
Uomar <uomarearlie7@gmail.com>
2019-07-10 19:29:05 +0300

    created the base template to be inherited all over the app

--- neighb/urls.py
+++ neighb/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^$',views.home,name='home'),
     url(r'^api/comment/$',views.ReviewList.as_view()),
     url(r'^api/home/$',views.HomeList.as_view()),
     url(r'^api/profile/$',views.ProfileList.as_view()),
