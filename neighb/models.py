00784f3fef38eb8c67bb5ad25e229e85b69a90e2
Uomar <uomarearlie7@gmail.com>
2019-07-10 14:15:47 +0300

    completed the models.

--- neighb/models.py
+++ neighb/models.py
@@ -8,6 +8,8 @@ from django.dispatch import receiver
 class Profile(models.Model):
     fname = models.CharField(max_length=40)
     lname = models.CharField(max_length=40)
+    id_no = models.IntegerField()
+    email = models.EmailField()
     username = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
     profile_pic = models.ImageField(upload_to='avatar/', blank=True)
@@ -33,9 +35,44 @@ class Profile(models.Model):
 
 class Home(models.Model):
     hname = models.CharField(max_length=66)
-    pic = models.ImageField(upload_to='houses/')
+    pic1 = models.ImageField(upload_to='houses/')
+    pic2 = models.ImageField(upload_to = 'houses/')
+    pic3 = models.ImageField(upload_to = 'houses/')
     location = models.CharField(max_length = 80)
     price = models.CharField(max_length = 80)
+    description = models.TextField()
     owner = models.ForeignKey(User, on_delete=models.CASCADE,related_name='houses')
 
+    def __str__(self):
+        return  self.hname
+    
+    def save_home(self):
+        self.save()
+    
+    def delete_home(self):
+        self.delete()
 
+class Review(models.Model):
+    comment = models.CharField(max_length = 120)
+    house = models.ForeignKey(Home, on_delete=models.CASCADE, related_name='comments')
+    posted = models.Foreignkey(User,)
+
+    def __str__(self):
+        retrn self.comment
+    
+    def save_comment(self):
+        self.save()
+
+    def delete_comment(self):
+        self.delete()
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(username=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
