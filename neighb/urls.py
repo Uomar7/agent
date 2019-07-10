bbd79f15effc373dea8cba4d44072f2fa41d29bd
Uomar <uomarearlie7@gmail.com>
2019-07-10 18:23:04 +0300

    created urls for the apiviews

---
+++ neighb/urls.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^api/comment/$',views.ReviewList.as_view()),
+    url(r'^api/home/$',views.HomeList.as_view()),
+    url(r'^api/profile/$',views.ProfileList.as_view()),
+]
