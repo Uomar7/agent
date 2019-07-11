56495ceb93a7e9711d363d3bc39e5772c9da2665
Uomar <uomarearlie7@gmail.com>
2019-07-11 11:22:19 +0300

    created the landing page

--- neighbour/urls.py
+++ neighbour/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('neighb.urls')),
 ]
-
 if settings.DEBUG:
+
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
